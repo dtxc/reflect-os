920ad7bdd120bb18f98e842910daf7ebbb05f403
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-24 22:38:12 +0300

    Update string.h

--- lib/string.h
+++ lib/string.h
@@ -5,6 +5,7 @@
 
 #define low_16(address) (uint16_t)((address) & 0xFFFF)
 #define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
+#define null 0
 
 int string_length(char *s);
 void reverse(char s[]);
@@ -18,6 +19,5 @@ void split_string(char str[], char delimiter, int n, char arr[]);
 int get_index(char *str, char what);
 char *remove(char *str, char *what);
 void remove_by_index(char str[], char dest[], int i);
-//void split(char *str, char *dest, char delimiter, int n);
 int split(char *str, char delimiter, char ***arr);
 void remove_item(char *arr, int i, int len);
