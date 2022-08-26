437522bff574fe4ba222c12a1ce1bc4698c3fba8
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-26 18:08:01 +0300

    added colors for later use

--- drivers/display.h
+++ drivers/display.h
@@ -7,6 +7,25 @@
 #define REG_SCREEN_CTRL 0x3d4
 #define REG_SCREEN_DATA 0x3d5
 
+typedef enum {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    BROWN = 6,
+    LIGHT_GREY = 7,
+    DARK_GREY = 8,
+    LIGHT_BLUE = 9,
+    LIGHT_GREEN = 10,
+    LIGHT_CYAN = 11,
+    LIGHT_RED = 12,
+    LIGHT_MAGENTA = 13,
+    LIGHT_BROWN = 14,
+    WHITE = 15
+} colors_t;
+
 void print_string(char *string);
 void print_nl();
 void clear_screen();
