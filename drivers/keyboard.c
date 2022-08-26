3121dcb109484899ab1b6d6d3739eb92087e9461
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-26 10:09:07 +0300

    update
    added symbols in shift ascii array and added command history that can be accessed using up arrow

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -9,9 +9,13 @@
 
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
-#define UP_ARROW 0x4C
+#define UP_ARROW 0x48
+#define DOWN_ARROW 0x50
 #define SHIFT 0x2A
+#define RSHIFT 0x36
 #define SHIFT_RELEASE 0xAA 
+#define RSHIFT_RELEASE 0xB6
+#define CAPS_LOCK 0x3A
 
 static char key_buffer[256];
 static bool shift;
@@ -22,14 +26,14 @@ const char *sc_name[] = {"ERROR", "Esc", "1", "2", "3", "4", "5", "6",
                          "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`",
                          "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
                          "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
-const char sc_ascii_shift[] = {'?', '?', '1', '2', '3', '4', '5', '6',
-                         '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y',
-                         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G',
-                         'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V',
-                         'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
+const char sc_ascii_shift[] = {'?', '?', '!', '@', '#', '$', '%', '^',
+                         '&', '*', '(', ')', '_', '+', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y',
+                         'U', 'I', 'O', 'P', '{', '}', '?', '?', 'A', 'S', 'D', 'F', 'G',
+                         'H', 'J', 'K', 'L', ':', '\"', '~', '/', '|', 'Z', 'X', 'C', 'V',
+                         'B', 'N', 'M', '<', '>', '?', '?', '?', '?', ' '};
 const char sc_ascii[] = {'?', '?', '1', '2', '3', '4', '5', '6',
                          '7', '8', '9', '0', '-', '=', '?', '?', 'q', 'w', 'e', 'r', 't', 'y',
-                         'u', 'i', 'o', 'p', '[', ']', '?', '>', 'a', 's', 'd', 'f', 'g',
+                         'u', 'i', 'o', 'p', '[', ']', '?', '?', 'a', 's', 'd', 'f', 'g',
                          'h', 'j', 'k', 'l', ';', '\'', '`', '?', '\\', 'z', 'x', 'c', 'v',
                          'b', 'n', 'm', ',', '.', '/', '?', '?', '?', ' '};
 
@@ -41,10 +45,10 @@ static void keyboard_callback(registers_t *regs) {
     status = port_byte_in(0x64);
     if (status & 0x01) {
         scancode = port_byte_in(0x60);
-        if (scancode == SHIFT || scancode == 54) {
+        if (scancode == SHIFT || scancode == RSHIFT) {
             shift = true;
         }
-        if (scancode == SHIFT_RELEASE || scancode == 0xB6) {
+        if (scancode == SHIFT_RELEASE || scancode == RSHIFT_RELEASE) {
             shift = false;
         }
         if (scancode == BACKSPACE) {
@@ -52,6 +56,22 @@ static void keyboard_callback(registers_t *regs) {
                 print_backspace();
             }
         }
+        if (scancode == UP_ARROW) {
+            if (string_length(key_buffer) != 0) return;
+            for (int i = 0; i < string_length(get_previous_command()); i++) {
+                append(key_buffer, get_previous_command()[i]);
+            }
+            print_string(get_previous_command());
+        }
+        if (scancode == DOWN_ARROW) {
+            if (compare_string(key_buffer, get_previous_command()) == 0) {
+                for (int i = 0; i < string_length(key_buffer); i++) {
+                    print_backspace();
+                }
+                key_buffer[0] = '\0';
+            }
+        }
+        if (scancode == CAPS_LOCK) shift = !shift;
         if (scancode == ENTER) {
             if (string_length(key_buffer) == 0) {
                 print_nl();
@@ -60,6 +80,10 @@ static void keyboard_callback(registers_t *regs) {
                 execute_command(key_buffer);
                 key_buffer[0] = '\0';
             }
+        }
+        if (scancode == 0x47 && shift == true) {
+            append(key_buffer, '?');
+            print_string("?");
         }
         if (scancode <= 57 && sc_ascii[scancode] != '?' && shift == false) {
             char letter = sc_ascii[(int) scancode];
