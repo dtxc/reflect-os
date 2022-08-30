5a7c535eddb6dfc15ee00a1bfd2ffef14cad7d83
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-30 21:34:26 +0300

    Update math.h

--- lib/math.h
+++ lib/math.h
@@ -1,25 +1,15 @@
 #pragma once
 
-#include <stdint.h>
+#include "../kernel/core/types.h"
 
 #define e 2.71828182845
 #define pi 3.14159265359
+#define nan 0x7F800001
+#define TAYLOR_SERIES_N 10
 
-typedef struct {
-    float Re, Im;
-} complex32_t;
-
-typedef struct {
-    double Re, Im;
-} complex64_t;
-
-float add(float x, float y);
-float sub(float x, float y);
-float mult(float x, float y);
-float div(float x, float y); 
 float abs(float x);
 float sin(float x);
-float cos(float x); //sine and cosine are defined using taylor expansion
+float cos(float x);
 float tan(float x);
 float sec(float x);
 float csc(float x);
@@ -27,7 +17,21 @@ float cot(float x);
 float sinh(int x);
 float cosh(int x);
 float tanh(int x);
-uint64_t factorial(uint16_t x);
+u64 factorial(u16 x);
 float pow(float x, int y);
 float exp(int x);
-//uint64_t perm(uint16_t x, uint16_t y);
+complex32_t ln(float x);
+complex32_t log(float x, float base);
+complex64_t sqrt(double x);
+float asin(float x);
+float acos(float x);
+float atan(float x);
+float acsc(float x);
+float asec(float x);
+float acot(float x);
+float asinh(float x);
+complex32_t acosh(float x);
+complex32_t atanh(float x);
+float acsch(float x);
+float asech(float x);
+float acoth(float x);
