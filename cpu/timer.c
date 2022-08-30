c295f15d9bcf39bec5a3f28e33a9ce53f532190e
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-30 21:33:30 +0300

    Update timer.c

--- cpu/timer.c
+++ cpu/timer.c
@@ -1,10 +1,6 @@
-#include "timer.h"
-#include "../drivers/display.h"
-#include "../drivers/ports.h"
-#include "../lib/string.h"
-#include "isr.h"
+#include "../kernel/kernel.h"
 
-uint32_t tick = 0;
+u32 tick = 0;
 
 static void timer_callback(registers_t *regs) {
     tick++;
@@ -16,12 +12,12 @@ static void timer_callback(registers_t *regs) {
     print_nl();
 }
 
-void init_timer(uint32_t freq) {
+void init_timer(u32 freq) {
     register_interrupt_handler(IRQ0, timer_callback);
 
-    uint32_t divisor = 1193180 / freq;
-    uint8_t low  = (uint8_t)(divisor & 0xFF);
-    uint8_t high = (uint8_t)( (divisor >> 8) & 0xFF);
+    u32 divisor = 1193180 / freq;
+    u8 low  = (u8)(divisor & 0xFF);
+    u8 high = (u8)( (divisor >> 8) & 0xFF);
     port_byte_out(0x43, 0x36);
     port_byte_out(0x40, low);
     port_byte_out(0x40, high);
