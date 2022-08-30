c255b85153dae10d3cd885d4b42a493a3974dd68
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-30 10:17:48 +0300

    Update keyboard.h

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,3 +1,32 @@
 #pragma once
 
+#define HOME 0x47
+#define END 0x4F
+
+#define SYSRQ 0x54
+#define BACKSPACE 0x0E
+#define ENTER 0x1C
+
+#define SHIFT 0x2A
+#define RSHIFT 0x36
+#define CAPS_LOCK 0x3A
+#define SHIFT_RELEASE 0xAA 
+#define RSHIFT_RELEASE 0xB6
+
+#define UP_ARROW 0x48
+#define DOWN_ARROW 0x50
+#define RIGHT_ARROW 0x4D
+#define LEFT_ARROW 0x4B
+
+#define FUNCTION1 0x3B
+#define FUNCTION2 0x3C
+#define FUNCTION3 0x3D
+#define FUNCTION4 0x3E
+#define FUNCTION5 0x3F
+#define FUNCTION6 0x40
+#define FUNCTION7 0x41
+#define FUNCTION8 0x42
+#define FUNCTION9 0x43
+#define FUNCTION10 0x44
+
 void init_keyboard();
