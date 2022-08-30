1dbd74412900b897d6d8adb6cb1a7f0f1035f2b0
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-30 21:29:08 +0300

    Update mem.h

--- lib/mem.h
+++ lib/mem.h
@@ -1,22 +1,14 @@
 #pragma once
 
-#include <stdint.h>
-#include <stddef.h>
+#include "../kernel/core/types.h"
 
 #define NULL_POINTER ((void*)0)
 
-typedef enum {
-    CHAR,
-    INT,
-    VOID,
-    PCHAR
-} TYPE;
-
-void memory_copy(uint8_t* source, uint8_t* dest, uint32_t nbytes);
+void memory_copy(u8* source, u8* dest, u32 nbytes);
 void init_dynamic_mem();
 void test_dynamic_mem();
 void print_dynamic_node_size();
 void print_dynamic_mem();
 void *mem_alloc(size_t size);
 void mem_free(void* p);
-void* alloc(TYPE t, int n);
+void* alloc(TYPE_t t, int n);
