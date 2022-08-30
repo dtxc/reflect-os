ceb7c3d495a12fbfd2c32ed88c2ae160fa770054
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-30 10:16:38 +0300

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,6 +1,10 @@
 #pragma once
 
+extern void shutdown();
+extern void halt();
+
 void reboot();
+void shutdown();
 void execute_command(char *input);
 char *get_previous_command();
 void save_command(char *command);
