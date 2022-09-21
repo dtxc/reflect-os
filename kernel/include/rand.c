15caecf5387c855187a689c237617122736c4db0
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-09-21 20:12:20 +0300

    add rand library
    added a library for generating random numbers and stuff
    
    note: numbers generated aren't truly random, they are pseudorandom

---
+++ kernel/include/rand.c
@@ -0,0 +1,37 @@
+/*
+    Copyright (c) 2022 thatOneArchUser
+    All rights reserverd
+*/
+
+#include "rand.h"
+#include "string.h"
+#include "timer.h"
+#include "stdlib.h"
+
+static u32 next = 1;
+static const int a = 1103515245;
+static const int b = 69420;  //this number can be anything
+static const int m = 0x80000000; //max value
+
+u32 rand() { 
+    if (next == 0) {
+        next = gettick();
+    }
+    next = (a * next + b) % m; //https://en.wikipedia.org/wiki/Pseudorandom_number_generator
+    return next;
+}
+
+void srand(u32 seed) {
+    next = seed;
+}
+
+/*
+char *rand_string(u16 len, char *arr) { //not working very well
+    if (strlen(arr) == 0) return nullptr;
+    char *res;
+    for (u16 i = 0; i < len; i++) {
+        res[i] = arr[rand() % (strlen(arr) - 1)];
+    }
+    return res;
+}
+*/
---
+++ kernel/include/rand.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2022 thatOneArchUser
+    All rights reserverd
+*/
+
+#pragma once
+
+#include "types.h"
+
+u32 rand();
+void srand(u32 seed);
+//char *rand_string(u16 len, char *arr);
