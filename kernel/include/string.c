edb4067825287767c13d5c77f400faac4f85bde2
Archie <69575053+thatOneArchUser@users.noreply.github.com>
2022-09-23 21:49:20 +0300

    Add files via upload

--- kernel/include/string.c
+++ kernel/include/string.c
@@ -4,20 +4,13 @@
 */
 
 #include "mem.h"
+#include "assert.h"
 #include "stdio.h"
 #include "types.h"
 #include "stdlib.h"
+#include "string.h"
 
-void reverse(char s[]) {
-    int c, i, j;
-    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
-}
-
-void append(char s[], char n) {
+void append(char *s, char n) {
     int len = strlen(s);
     s[len] = n;
     s[len+1] = '\0';
@@ -43,50 +36,89 @@ bool startswith(char str[], char what[]) {
     return true;
 }
 
-int split(char *str, char delimiter, char ***dest) {
-    int count = 1;
-    int token_len = 1;
-    int i = 0;
-    char *p;
-    char *t;
+char *strtok(char *s, char *delm) {
+    int q = 0;
 
-    p = str;
-    while (*p != '\0') {
-        if (*p == delimiter) count++;
-        p++;
-    }
+    if (!s || !delm || s[q] == '\0') return nullptr;
 
-    *dest = (char **) malloc(count);
-    if (*dest == 0) return 1;
-    p = str;
-    while (*p != '\0') {
-        if (*p == delimiter) {
-            (*dest)[i] = (char *) malloc(count);
-            if ((*dest)[i] == 0) return 1;
-            token_len = 0;
-            i++;
+    char *c = (char*) malloc(sizeof(char) * 100);
+    int i = q;
+    int j = 0;
+    int k = 0;
+
+    while (s[i] != '\0') {
+        j = 0;
+        while (delm[j] != '\0') {
+            if (s[i] != delm[j]) {
+                c[k] = s[i];
+            } else {
+                c[i] = 0;
+                q = i + 1;
+                return c;
+            }
+            j++;
         }
-        p++;
-        token_len++;
-    }
-    (*dest)[i] = (char *) malloc(token_len);
-    if ((*dest)[i] == 0) return 1;
-    i = 0;
-    p = str;
-    t = ((*dest)[i]);
-    while (*p != '\0') {
-        if (*p != delimiter && *p != '\0') {
-            *t = *p;
-            t++;
-        } else {
-            *t = '\0';
-            i++;
-            t = ((*dest)[i]);
-        }
-        p++;
+        i++;
+        k++;
     }
 
-    return count;
+    //should never reach here
+    return nullptr;
+}
+
+char *strdup(char *token) {
+    char *str;
+    char *ptr;
+    int len = 0;
+
+    while (token[len]) {
+        len++;
+    }
+    str = malloc(len + 1);
+    ptr = str;
+    while (*token) {
+        *ptr = *token++;
+    }
+    *ptr = '\0';
+
+    return str;
+}
+
+char **split(char *str, char delm) {
+    char **res;
+    size_t count;
+    char *tmp;
+    char *last;
+    char delim[2];
+    delim[0] = delm;
+    delim[1] = 0;
+
+    while (*tmp) {
+        if (delm == *tmp) {
+            count++;
+            last = tmp;
+        }
+        tmp++;
+    }
+
+    count += last < (str + strlen(str) - 1);
+    count++;
+
+    res = malloc(sizeof(char*) * count);
+    if (res) {
+        size_t idx = 0;
+        char *token = strtok(str, delim);
+
+        while (token) {
+            ASSERT(idx < count);
+            *(res + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        ASSERT(idx == count - 1);
+        *(res + idx) = 0;
+    }
+
+    return res;
 }
 
 char *trim(char *str) {
--- kernel/include/string.h
+++ kernel/include/string.h
@@ -9,23 +9,15 @@
 
 #define low_16(address) (u16)((address) & 0xFFFF)
 #define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
+#define STRING_LOWERCASE "abcdefghijklmnopqrstuvwxyz"
+#define STRING_UPPERCASE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define STRING_LETTERS "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define STRING_DIGITS "0123456789"
 
-int string_length(char *s);
-void reverse(char s[]);
-void int_to_string(int n, char str[]);
-bool backspace(char s[]);
-bool qq(char s[]);
-void append(char s[], char n);
-bool compare_string(char s1[], char s2[]);
+bool backspace(char *s);
+void append(char *s, char n);
 bool startswith(char str[], char what[]);
-int find_matches(char str[], char what);
-void split_string(char str[], char delimiter, int n, char arr[]);
-int get_index(char *str, char what);
-char *remove(char *str, char *what);
-void remove_by_index(char str[], char dest[], int i);
-int split(char *str, char delimiter, char ***dest);
-void remove_item(char *arr, int i, int len);
-char *to_lowercase(char *str);
-int string_to_int(char *str);
-int arr_len(char **arr);
+char *strtok(char *s, char *delm);
+char *strdup(char *token);
+char **split(char *str, char delm);
 char *trim(char *str);
