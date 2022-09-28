0580b066653b01237dcf5000e3348909910e646b
Isaac Dee Schnutz <69575053+thatOneArchUser@users.noreply.github.com>
2022-09-28 20:50:42 +0300

    Update stdlib.h

--- kernel/include/stdlib.h
+++ kernel/include/stdlib.h
@@ -11,7 +11,7 @@ void memset(void *addr, u8 val, int num);
 void memcpy(void *addr, void *source, int num);
 int strcpy(char *dest, char *source);
 bool strcmp(char *str1, char *str2);
-int strlen(char *str);
+u32 strlen(char *str);
 int int2str(char *dest, int num);
 int int2hex(char *dest, int num);
 void sprintf_args(char *dest, char *str, void *ptr);
